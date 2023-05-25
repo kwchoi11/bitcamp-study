ccfd3443d3e5c89de0f4178642a2a376d69aca02
kwchoi11 <kwchoi0216@gmail.com>
2023-05-25 10:04:45 +0900

    /

---
+++ Test4.java
@@ -0,0 +1,4 @@
+public class Test4 {}
+class F {}
+class G {}
+//public은 한 소스 파일에 하나만 있을 수 있다. 
---
+++ java-lang/app/src/main/java/bitcamp/Test1.java
@@ -0,0 +1,8 @@
+package bitcamp;
+
+class Test1 {
+  public static void main(String[] args) {
+    
+  }
+}
+
