61e0e13b67890559ce77cd64533af5e735fa9853
kwchoi11 <kwchoi0216@gmail.com>
2023-05-30 15:41:54 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,18 +1,329 @@
 package bitcamp.myapp;
 
+// 코드 본문에서 사용할 클래스가 어떤 패키지의 클래스인지 지정한다. 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
     System.out.println("나의 목록 관리 시스템");
     System.out.println("-------------------------------------");
 
-    int no = 100;
-    String name = "홍길동";
-    int age = 20;
-    boolean working = true;
-    char gender = 'M';
-    float leftEye = 1.5f;
-    float rightEye = 1.0f;
+    // 키보드 스캐너 준비
+    Scanner scanner = new java.util.Scanner(System.in);
+
+    {
+      System.out.print("번호? ");
+      int no = scanner.nextInt();
+
+      System.out.print("이름? ");
+      String name = scanner.next();
+
+      System.out.print("나이? ");
+      int age = scanner.nextInt();
+
+      System.out.print("재직중? ");
+      boolean working = scanner.nextBoolean();
+
+      System.out.print("성별(남자:M, 여자: W)? ");
+      String str = scanner.next();
+      char gender = str.charAt(0);
+
+      System.out.print("시력(왼쪽, 오른쪽)? ");
+      float leftEye = scanner.nextFloat();
+      float rightEye = scanner.nextFloat();
+    }
+
+    System.out.print("번호? ");
+    int no1 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name1 = scanner.next();
+
+    System.out.print("나이? ");
+    int age1 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working1 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender1 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye1 = scanner.nextFloat();
+    float rightEye1 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no2 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name2 = scanner.next();
+
+    System.out.print("나이? ");
+    int age2 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working2 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender2 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye2 = scanner.nextFloat();
+    float rightEye2 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no3 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name3 = scanner.next();
+
+    System.out.print("나이? ");
+    int age3 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working3 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender3 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye3 = scanner.nextFloat();
+    float rightEye3 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no4 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name4 = scanner.next();
+
+    System.out.print("나이? ");
+    int age4 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working4 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender4 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye4 = scanner.nextFloat();
+    float rightEye4 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no5 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name5 = scanner.next();
+
+    System.out.print("나이? ");
+    int age5 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working5 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender5 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye5 = scanner.nextFloat();
+    float rightEye5 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no6 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name6 = scanner.next();
+
+    System.out.print("나이? ");
+    int age6 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working6 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender6 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye6 = scanner.nextFloat();
+    float rightEye6 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no7 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name7 = scanner.next();
+
+    System.out.print("나이? ");
+    int age7 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working7 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender7 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye7 = scanner.nextFloat();
+    float rightEye7 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no8 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name8 = scanner.next();
+
+    System.out.print("나이? ");
+    int age8 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working8 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender8 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye8 = scanner.nextFloat();
+    float rightEye8 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no9 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name9 = scanner.next();
+
+    System.out.print("나이? ");
+    int age9 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working9 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender9 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye9 = scanner.nextFloat();
+    float rightEye9 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no10 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name10 = scanner.next();
+
+    System.out.print("나이? ");
+    int age10 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working10 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender10 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye10 = scanner.nextFloat();
+    float rightEye10 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no11 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name11 = scanner.next();
+
+    System.out.print("나이? ");
+    int age11 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working11 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender11 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye11 = scanner.nextFloat();
+    float rightEye11 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no12 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name12 = scanner.next();
+
+    System.out.print("나이? ");
+    int age12 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working12 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender12 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye12 = scanner.nextFloat();
+    float rightEye12 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no13 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name13 = scanner.next();
+
+    System.out.print("나이? ");
+    int age13 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working13 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender13 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye13 = scanner.nextFloat();
+    float rightEye13 = scanner.nextFloat();
+
+    System.out.print("번호? ");
+    int no14 = scanner.nextInt();
+
+    System.out.print("이름? ");
+    String name14 = scanner.next();
+
+    System.out.print("나이? ");
+    int age14 = scanner.nextInt();
+
+    System.out.print("재직중? ");
+    boolean working14 = scanner.nextBoolean();
+
+    System.out.print("성별(남자:M, 여자: W)? ");
+    str = scanner.next();
+    char gender14 = str.charAt(0);
+
+    System.out.print("시력(왼쪽, 오른쪽)? ");
+    float leftEye14 = scanner.nextFloat();
+    float rightEye14 = scanner.nextFloat();
+
+    System.out.println("----------------------------------------");
+
+    // int no = 100;
+    // String name = "홍길동";
+    // int age = 20;
+    // boolean working = true;
+    // char gender = 'M';
+    // float leftEye = 1.5f;
+    // float rightEye = 1.0f;
 
     System.out.printf("번호: %d\n", no);
     System.out.printf("이름: %s\n", name);
@@ -21,26 +332,126 @@ public class App {
     System.out.printf("성별(남자(M), 여자(W)): %c\n", gender);
     System.out.printf("좌우시력: %.1f,%.1f\n", leftEye, rightEye);
 
+    System.out.printf("번호: %d\n", no1);
+    System.out.printf("이름: %s\n", name1);
+    System.out.printf("나이: %d\n", age1);
+    System.out.printf("재직자: %b\n", working1);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender1);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye1, rightEye1);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no3);
+    System.out.printf("이름: %s\n", name3);
+    System.out.printf("나이: %d\n", age3);
+    System.out.printf("재직자: %b\n", working3);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender3);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye3, rightEye3);
+
+    System.out.printf("번호: %d\n", no4);
+    System.out.printf("이름: %s\n", name4);
+    System.out.printf("나이: %d\n", age4);
+    System.out.printf("재직자: %b\n", working4);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender4);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye4, rightEye4);
+
+    System.out.printf("번호: %d\n", no5);
+    System.out.printf("이름: %s\n", name5);
+    System.out.printf("나이: %d\n", age5);
+    System.out.printf("재직자: %b\n", working5);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender5);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye5, rightEye5);
+
+    System.out.printf("번호: %d\n", no6);
+    System.out.printf("이름: %s\n", name6);
+    System.out.printf("나이: %d\n", age6);
+    System.out.printf("재직자: %b\n", working6);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender6);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye6, rightEye6);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    System.out.printf("번호: %d\n", no2);
+    System.out.printf("이름: %s\n", name2);
+    System.out.printf("나이: %d\n", age2);
+    System.out.printf("재직자: %b\n", working2);
+    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+    scanner.close();
     // System.out.print("번호: ");
     // System.out.println(100);
 
     // System.out.print("번호: ");
     // System.out.println(100);
-    
+
     // System.out.printf("이름: %s", "홍길동"); //java에서 print, println, printf 차이점 찾아보기.
     // System.out.println();
 
     // System.out.println("나이: " + 20);
-    
+
     // System.out.printf("재직자: %b\n", true); //'\n' 과 같은 문자를 escape character라고 부른다.
 
-    // System.out.printf("성별(남자(M), 여자(W)): %c\n", 'M'); //"M"과 같이 문자열 처리""를 하면 오류 발생. 
+    // System.out.printf("성별(남자(M), 여자(W)): %c\n", 'M'); //"M"과 같이 문자열 처리""를 하면 오류
+    // 발생.
 
     // System.out.printf("좌우시력: %.1f,%.1f\n", 1.5f, 1.0f);
   }
 }
-//System  .  out  .  println            ("홍길동");
-//상자       도구       기능(function)   기능 수행에 필요한 값
-//클래스     객체      메소드(method)     argument, parameter
-//class    object
-//         instance
+// System . out . println ("홍길동");
+// 상자 도구 기능(function) 기능 수행에 필요한 값
+// 클래스 객체 메소드(method) argument, parameter
+// class object
+// instance
