948a67cfb8f2ecfd3a78dba02e5171eb713b6b8b
kwchoi11 <kwchoi0216@gmail.com>
2023-06-01 15:29:43 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
-    printTitle();
 
     // 키보드 스캐너 준비
     Scanner scanner = new java.util.Scanner(System.in);
@@ -16,15 +15,14 @@ public class App {
     int userId = 1;
     int length = 0;
 
-    // int i = 0;
-
     int[] no = new int[MAX_SIZE];
     String[] name = new String[MAX_SIZE];
     String[] email = new String[MAX_SIZE];
     String[] password = new String[MAX_SIZE];
     char[] gender = new char[MAX_SIZE];
 
-    // 회원정보 등록
+    printTitle();
+
     for (int i = 0; i < MAX_SIZE; i++) {
       // while (i < 3) {
       // i = i + 1;
@@ -32,48 +30,15 @@ public class App {
       // i++;
 
       inputMember(scanner, i, name, email, password, gender, no, userId++);
-
       length++;
-
       if (!promptContinue(scanner)) {
         break;
       }
     }
 
-    System.out.println("----------------------------------------");
-
-    // int no = MAX_SIZE;
-    // String name = "홍길동";
-    // int age = 20;
-    // boolean working = true;
-    // char gender = 'M';
-    // float leftEye = 1.5f;
-    // float rightEye = 1.0f;
+    printMembers(length, no, name, email, gender);
     
-    System.out.println("번호, 이름, 이메일, 성별\n");
-    System.out.println("----------------------------------------");
-
-    for (int i = 0; i < length; i++) {
-      System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
-    }
     scanner.close();
-    // System.out.print("번호: ");
-    // System.out.println(MAX_SIZE);
-
-    // System.out.print("번호: ");
-    // System.out.println(MAX_SIZE);
-
-    // System.out.printf("이름: %s", "홍길동"); //java에서 print, println, printf 차이점 찾아보기.
-    // System.out.println();
-
-    // System.out.println("나이: " + 20);
-
-    // System.out.printf("재직자: %b\n", true); //'\n' 과 같은 문자를 escape character라고 부른다.
-
-    // System.out.printf("성별(남자(M), 여자(W)): %c\n", 'M'); //"M"과 같이 문자열 처리""를 하면 오류
-    // 발생.
-
-    // System.out.printf("좌우시력: %.1f,%.1f\n", 1.5f, 1.0f);
   }
 
   static void printTitle() {
@@ -100,19 +65,6 @@ public class App {
         String menuNo = scanner.next();
         scanner.nextLine(); // 입력 값(token)을 읽고 난 후에 남아 있는 줄바꿈 코드를 제거한다. 
 
-
-        // if문 사용
-        // if (menuNo.equals("1")) {
-        //   gender[i] = 'M';
-        //   break;
-        // } else if (menuNo.equals("2")) {
-        //   gender[i] = 'W';
-        //   break;
-        // } else { 
-        //   System.out.println("무효한 번호입니다.");
-        // }
-
-        // switch문 사용
         switch (menuNo) {
           case "1": 
             gender[i] = 'M';
@@ -124,16 +76,11 @@ public class App {
             System.out.println("무효한 번호입니다.");
         }
       }
-
-      // gender[i] = scanner.next().charAt(0);
-
       no[i] = userId++;
-
   }
 
   static boolean promptContinue(Scanner scanner) {
     System.out.print("계속 하시겠습니까?(Y/n)");
-      // scanner.nextLine(); // 이전 next()를 실행한 후 남아있는 줄바꿈 코드를 제거하기 위함.
       String response = scanner.nextLine();
       if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
         return false;
@@ -141,9 +88,12 @@ public class App {
       return true;
   }
 
+  static void printMembers(int length, int[] no, String[] name, String[] email, char[] gender) {
+    System.out.println("----------------------------------------");
+    System.out.println("번호, 이름, 이메일, 성별\n");
+    System.out.println("----------------------------------------");
+    for (int i = 0; i < length; i++) {
+      System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
+    }
+  }
 }
-// System . out . println ("홍길동");
-// 상자 도구 기능(function) 기능 수행에 필요한 값
-// 클래스 객체 메소드(method) argument, parameter
-// class object
-// instance 
