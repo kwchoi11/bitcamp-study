f064b71427b73ca322b1077f26d2f09a70c2bf69
kwchoi11 <kwchoi0216@gmail.com>
2023-06-01 18:00:46 +0900

    .

---
+++ myapp/app/src/main/java/bitcamp/myapp/app01.java
@@ -0,0 +1,12 @@
+package bitcamp.myapp;
+
+public class app01 {
+  public String getGreeting() {
+    return "Hello World!";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new app01().getGreeting());
+  }
+}
+
---
+++ myapp/app/src/main/java/bitcamp/myapp/app03.java
@@ -0,0 +1,14 @@
+package bitcamp.myapp;
+
+public class app03 {
+  public static void main(String[] args) {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("---------------------------------");
+
+    int no = 100;
+    String name = "홍길동";
+    
+
+    System.out.printf("번호: %d\n", no);
+  }
+}
