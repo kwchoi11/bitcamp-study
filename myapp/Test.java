9303908e2e97aa1363d463b65ab4dfed6804b54f
kwchoi11 <kwchoi0216@gmail.com>
2023-06-01 13:09:44 +0900

    .

---
+++ myapp/Test.java
@@ -0,0 +1,8 @@
+package bitcamp.myapp;
+
+public class Test {
+  public static void mian(String[] args) {
+    int[] no = new int[]{100, 200, 300};
+    System.out.println(no.length);
+  }
+}
--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -13,69 +13,101 @@ public class App {
     // 키보드 스캐너 준비
     Scanner scanner = new java.util.Scanner(System.in);
 
-    // int count = 0;
+    final int MAX_SIZE = 100;
+    int userId = 1;
+    int length = 0;
 
-    int[] no = new int[3];
-    String[] name = new String[3];
-    int[] age = new int[3];
-    boolean[] working = new boolean[3];
-    char[] gender = new char[3];
-    float[] leftEye = new float[3];
-    float[] rightEye = new float[3];
+    // int i = 0;
 
-    for (int count = 0; count < 3; count++) {
-      // while (count < 3) {
-      // count = count + 1;
-      // count += 1;
-      // count++;
+    int[] no = new int[MAX_SIZE];
+    String[] name = new String[MAX_SIZE];
+    String[] email = new String[MAX_SIZE];
+    String[] password = new String[MAX_SIZE];
+    char[] gender = new char[MAX_SIZE];
 
-      System.out.print("번호? ");
-      no[count] = scanner.nextInt();
+    // 회원정보 등록
+    for (int i = 0; i < MAX_SIZE; i++) {
+      // while (i < 3) {
+      // i = i + 1;
+      // i += 1;
+      // i++;
 
       System.out.print("이름? ");
-      name[count] = scanner.next();
+      name[i] = scanner.next();
 
-      System.out.print("나이? ");
-      age[count] = scanner.nextInt();
+      System.out.print("이메일? ");
+      email[i] = scanner.next();
 
-      System.out.print("재직중? ");
-      working[count] = scanner.nextBoolean();
+      System.out.print("암호? ");
+      password[i] = scanner.next();
 
-      System.out.print("성별(남자:M, 여자: W)? ");
-      String str = scanner.next();
-      gender[count] = str.charAt(0);
+      loop: while (true) {
+        System.out.println("성별: ");
+        System.out.println(" 1. 남자");
+        System.out.println(" 2. 여자");
+        System.out.print("> ");
+        String menuNo = scanner.next();
 
-      System.out.print("시력(왼쪽, 오른쪽)? ");
-      leftEye[count] = scanner.nextFloat();
-      rightEye[count] = scanner.nextFloat();
+        // if문 사용
+        // if (menuNo.equals("1")) {
+        //   gender[i] = 'M';
+        //   break;
+        // } else if (menuNo.equals("2")) {
+        //   gender[i] = 'W';
+        //   break;
+        // } else {
+        //   System.out.println("무효한 번호입니다.");
+        // }
 
-      // count++;
+        // switch문 사용
+        switch (menuNo) {
+          case "1": 
+            gender[i] = 'M';
+            break loop;
+          case "2":
+            gender[i] = 'W';
+            break loop;
+          default:
+            System.out.println("무효한 번호입니다.");
+        }
+      }
+
+      // gender[i] = scanner.next().charAt(0);
+
+      no[i] = userId++;
+
+      length++;
+
+      System.out.print("계속 하시겠습니까?(Y/n)");
+      scanner.nextLine(); // 이전 next()를 실행한 후 남아있는 줄바꿈 코드를 제거하기 위함.
+      String response = scanner.nextLine();
+      if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
+        break;
+      }
     }
 
     System.out.println("----------------------------------------");
 
-    // int no = 100;
+    // int no = MAX_SIZE;
     // String name = "홍길동";
     // int age = 20;
     // boolean working = true;
     // char gender = 'M';
     // float leftEye = 1.5f;
     // float rightEye = 1.0f;
+    
+    System.out.println("번호, 이름, 이메일, 성별\n");
+    System.out.println("----------------------------------------");
 
-    for (int count = 0; count < 3; count++) {
-      System.out.printf("번호: %d\n", no[count]);
-      System.out.printf("이름: %s\n", name[count]);
-      System.out.printf("나이: %d\n", age[count]);
-      System.out.printf("재직자: %b\n", working[count]);
-      System.out.printf("성별(남자(M), 여자(W)): %c\n", gender[count]);
-      System.out.printf("좌우시력: %.1f,%.1f\n", leftEye[count], rightEye[count]);
+    for (int i = 0; i < length; i++) {
+      System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
     }
     scanner.close();
     // System.out.print("번호: ");
-    // System.out.println(100);
+    // System.out.println(MAX_SIZE);
 
     // System.out.print("번호: ");
-    // System.out.println(100);
+    // System.out.println(MAX_SIZE);
 
     // System.out.printf("이름: %s", "홍길동"); //java에서 print, println, printf 차이점 찾아보기.
     // System.out.println();
@@ -94,4 +126,4 @@ public class App {
 // 상자 도구 기능(function) 기능 수행에 필요한 값
 // 클래스 객체 메소드(method) argument, parameter
 // class object
-// instance
+// instance 
