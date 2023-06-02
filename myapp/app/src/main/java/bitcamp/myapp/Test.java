961c25e17cd34e843657ef8e334eeb0506048b61
kwchoi11 <kwchoi0216@gmail.com>
2023-06-02 12:21:52 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -2,6 +2,7 @@ package bitcamp.myapp;
 
 public class Test {
   public static void main(String[] args) {
+    System.out.println(args);
     int a = 100, b = 200;
     swap(a, b);
     System.out.printf("main(): %d, %d\n", a, b);
