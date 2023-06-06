d84af94592e781bba302bab0ba40b43f8eeabb6d
kwchoi11 <kwchoi0216@gmail.com>
2023-06-06 16:47:04 +0900

    .

--- PJ01/app/src/main/java/pj01/app_07/App07.java
+++ PJ01/app/src/main/java/pj01/app_07/App07.java
@@ -1,3 +1,34 @@
+package pj01;
+
+import java.util.Scanner;
+
 public class App07 {
-  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    final int MAX_SIZE = 1;
+    int length = 0;
+
+    String[] tableNo = new String[MAX_SIZE];
+    String[] personsNo = new String[MAX_SIZE];
+    String[] food1 = new String[MAX_SIZE];
+    String[] food2 = new String[MAX_SIZE];
+    String[] soju = new String[MAX_SIZE];
+    String[] beer = new String[MAX_SIZE];
+
+    printTitle();
+
+    for (int i = 0; i < MAX_SIZE; i++) {
+      inputMember(scanner, i, tableNo, personsNo, food1, food2, soju, beer);
+      length++;
+      if (!promptContinue(scanner)) {
+        break;
+      }
+    }
+
+    printMembers(length, tableNo, personsNo, food1, food2, soju, beer);
+
+    scanner.close();
+
+  }
 }
