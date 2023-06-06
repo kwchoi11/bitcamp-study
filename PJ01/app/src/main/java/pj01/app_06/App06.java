0e5c9ffe5857ac172afa98c8a3e37c8caba189e8
kwchoi11 <kwchoi0216@gmail.com>
2023-06-06 16:39:35 +0900

    .

--- PJ01/app/src/main/java/pj01/app_06/App06.java
+++ PJ01/app/src/main/java/pj01/app_06/App06.java
@@ -9,8 +9,7 @@ public class App06 {
 
     Scanner scanner = new Scanner(System.in);
 
-    final int MAX_SIZE = 3;
-    int userId = 1;
+    final int MAX_SIZE = 1;
     int length = 0;
 
     String[] tableNo = new String[MAX_SIZE];
@@ -77,15 +76,17 @@ public class App06 {
       personsNo[i] = scanner.next();
 
       loop1: while (true) {
-        System.out.println("안주를 선택해주세요.: ");
+        System.out.println("음식을 선택해주세요. ");
         System.out.println("  1. 오뎅탕 11.0");
         System.out.println("  2. 화채 8.0");
         System.out.println("  3. 떡볶이 9.0");
         System.out.println("  4. 감자튀김 10.0");
+        System.out.println("  5. 후라이드치킨 17.0");
+        System.out.println("  6. 양념치킨 18.0");
         System.out.println("> ");
-        String menuNo = scanner.next();
+        String menuNo1 = scanner.next();
 
-        switch (menuNo) {
+        switch (menuNo1) {
           case "1": 
             food1[i] = "오뎅탕";
             break loop1;
@@ -98,21 +99,122 @@ public class App06 {
           case "4":
             food1[i] = "감자튀김";
             break loop1;
+          case "5":
+            food1[i] = "후라이드치킨";
+            break loop1;
+          case "6":
+            food1[i] = "양념치킨";
+            break loop1;
           default:
             System.out.println("무효한 번호입니다. 다시 선택해 주세요.");
         }
       }
       
+      loop2: while (true) {
+        System.out.println("두 번째 음식을 선택해주세요.");
+        System.out.println("  1. 오뎅탕 11.0");
+        System.out.println("  2. 화채 8.0");
+        System.out.println("  3. 떡볶이 9.0");
+        System.out.println("  4. 감자튀김 10.0");
+        System.out.println("  5. 후라이드치킨 17.0");
+        System.out.println("  6. 양념치킨 18.0");
+        System.out.println("  7. 하나만 주문할게요.");
+        System.out.println("> ");
+        String menuNo2 = scanner.next();
 
+        switch (menuNo2) {
+          case "1":
+            food2[i] = "오뎅탕";
+            break loop2;
+          case "2":
+            food2[i] = "화채";
+            break loop2;
+          case "3":
+            food2[i] = "떡볶이";
+            break loop2;
+          case "4":
+            food2[i] = "감자튀김";
+            break loop2;
+          case "5":
+            food1[i] = "후라이드치킨";
+            break loop2;
+          case "6":
+            food1[i] = "양념치킨";
+            break loop2;
+          case "7":
+            food2[i] = "주문 없음";
+            break loop2;
+          default:
+            System.out.println("무효한 번호입니다. 다시 선택해주세요.");
+        }
+      }
+
+      loop3: while (true) {
+        System.out.println("소주를 선택해주세요. (종류 상관없이 6.0)");
+        System.out.println("  1. 진로");
+        System.out.println("  2. 참이슬");
+        System.out.println("  3. 처음처럼");
+        System.out.println("  4. 새로");
+        System.out.println("  5. 소주 주문 안 할래요.");
+        System.out.println("> ");
+        String sojuNo = scanner.next();
+
+        switch (sojuNo) {
+          case "1":
+            soju[i] = "진로";
+            break loop3;
+          case "2":
+            soju[i] = "참이슬";
+            break loop3;
+          case "3":
+            soju[i] = "처음처럼";
+            break loop3;
+          case "4":
+            soju[i] = "새로";
+            break loop3;
+          case "5":
+            soju[i] = "소주 주문 없음";
+            break loop3;
+          default:
+            System.out.println("무효한 번호입니다. 다시 선택해 주세요.");
+        }
+      }
+
+      loop4: while (true) {
+        System.out.println("맥주를 선택해주세요. (종류 상관없이 7.0)");
+        System.out.println("  1. 카스");
+        System.out.println("  2. 테라");
+        System.out.println("  3. 켈리");
+        System.out.println("  4. 맥주 주문 안 할래요.");
+        System.out.println("> ");
+        String beerNo = scanner.next();
+
+        switch (beerNo) {
+          case "1":
+            beer[i] = "카스";
+            break loop4;
+          case "2":
+            beer[i] = "테라";
+            break loop4;
+          case "3":
+            beer[i] = "켈리";
+            break loop4;
+          case "4":
+            beer[i] = "맥주 주문 없음";
+            break loop4;
+          default:
+            System.out.println("무효한 번호입니다. 다시 선택해 주세요.");
+        }
+      }
 
       length++;
 
-      System.out.print("계속 하시겠습니까?(Y/n) ");
-      scanner.nextLine();
-      String response = scanner.nextLine();
-      if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
-        break;
-      }
+      System.out.println("주문해주셔서 감사합니다! 맛있게 만들어드릴게요 :)");
+      // scanner.nextLine();
+      // String response = scanner.nextLine();
+      // if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
+      //   break;
+      // }
     }
 
     System.out.println("------------------------------------");
