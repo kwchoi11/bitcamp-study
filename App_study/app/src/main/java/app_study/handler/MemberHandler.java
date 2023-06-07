196b5f3435bd96b59929c42851b1cff7d2965080
kwchoi11 <kwchoi0216@gmail.com>
2023-06-07 20:08:15 +0900

    .

--- App_study/app/src/main/java/app_study/handler/MemberHandler.java
+++ App_study/app/src/main/java/app_study/handler/MemberHandler.java
@@ -1,5 +1,82 @@
 package app_study.handler;
 
+import app_study.util.Prompt;
+
 public class MemberHandler {
   
+  static final int MAX_SIZE = 100;
+  static in[] no = new int[MAX_SIZE];
+  static String[] name = new String[MAX_SIZE];
+  static String[] email = new String[MAX_SIZE];
+  static String[] password = new String[MAX_SIZE];
+  static char[] gender = new char[MAX_SIZE];
+  static int userID = 1;
+  static int length = 0;
+
+  static final char MALE = 'M';
+  static final char FEMALE = 'W';
+
+  public static void inputMember() {
+    if (!available()) {
+      System.out.println("더이상 입력할 수 없습니다.");
+      return;
+    }
+
+    name[length] = Prompt.inputString("이름? ");
+    email[length] = Prompt.inputString("이메일? ");
+    password[length] = Prompt.inputString("암호? ");
+    gender[length] = inputGender((char)0);
+
+    no[length] = userId++;
+    length++;
+  }
+
+  public static void printMembers() {
+    System.out.println("--------------------------------------");
+    System.out.println("번호, 이름, 이메일, 성별");
+    Systme.out.println("--------------------------------------");
+
+    for (int i = 0; i < length; i++) {
+      System.out.printf("%d, %s, %s, %s\n",
+      no[i], name[i], email[i],
+      toGenderString(gender[i]));
+    }
+  }
+
+  public static void viewMember() {
+    String memberNo = Prompt.inputString("번호? ");
+    for (int i = 0; i < length; i++) {
+      if (no[i] == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", name[i]);
+        System.out.printf("이메일: %s\n", email[i]);
+        System.out.printf("성별: %s\n", toGenderString(gender[i]));
+        return;
+      }
+    }
+    System.out.println("해당 번호의 회원이 없습니다.");
+  }
+
+  public static String toGenderString(char gender) {
+    return gender == 'M' ? "남성" : "여성";
+  }
+
+  public static void updateMember() {
+    String memberNo = Prompt.inputString("번호? ");
+    for (int i = 0; i < length; i++) {
+      if (no[i] == Integer.parseInt(memberNo)) {
+        System.out.printf("이름(%s)? ", name[i]);
+        name[i] = Prompt.inputString("");
+        System.out.printf("이메일(%s)? ", email[i]);
+        email[i] = Prompt.inputString("");
+        System.out.printf("새암호? ");
+        password[i] = Prompt.inputString("");
+        gender[i] = inputGender(gender[i]);
+        return;
+      }
+    }
+    System.out.println("해당 번호의 회원이 없습니다.");
+  }
+
+  
+
 }
