b8e24aa5f064523315f47229e51c3d69f934a90d
kwchoi11 <kwchoi0216@gmail.com>
2023-06-08 16:49:56 +0900

    .

---
+++ PJ01/app/src/main/java/pj01/app_10/App10.java
@@ -0,0 +1,51 @@
+package pj01.app_10;
+
+import pj01.app_10.handler.OrderHandler;
+import pj01.util.Prompt;
+
+public class App10 {
+  
+  public static void main(String[] args) {
+
+    printTitle();
+
+    printMenu();
+
+    while(true) {
+      String menuNo = Prompt.inputString("메인> ");
+      if (menuNo.equals("6")) {
+        break;
+      } else if (menuNo.equals("menu")) {
+        printMenu();
+      } else if (menuNo.equals("1")) {
+        OrderHandler.inputOrder();
+      } else if (menuNo.equals("2")) {
+        OrderHandler.printOrders();
+      } else if (menuNo.equals("3")) {
+        OrderHandler.viewOrder();
+      } else if (menuNo.equals("4")) {
+        OrderHandler.updateOrder();
+      } else if (menuNo.equals("5")) {
+        OrderHandler.deleteOrder();
+      } else {
+        System.out.println(menuNo);
+      }
+    }
+
+    Prompt.close();
+  }
+
+  static void printMenu() {
+    System.out.println("1. 주문등록");
+    System.out.println("2. 주문목록");
+    System.out.println("3. 주문조회");
+    System.out.println("4. 주문변경");
+    System.out.println("5. 주문삭제");
+    System.out.println("6. 종료");
+  }
+
+  static void printTitle() {
+    System.out.println("테이블별 주문 내역");
+    System.out.println("----------------------------------");
+  }
+}
---
+++ PJ01/app/src/main/java/pj01/app_10/vo/Order.java
@@ -0,0 +1,10 @@
+package pj01.app_10.vo;
+
+public class Order {
+  public String tableNo;
+  public String personsNo;
+  public String food1;
+  public String food2;
+  public String soju;
+  public String beer;
+}
