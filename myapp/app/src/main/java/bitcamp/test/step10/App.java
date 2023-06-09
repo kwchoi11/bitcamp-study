43ec879a99456b43efe578861387207985a1a43a
kwchoi11 <kwchoi0216@gmail.com>
2023-06-09 15:59:24 +0900

    .

--- myapp/app/src/main/java/bitcamp/test/step10/App.java
+++ myapp/app/src/main/java/bitcamp/test/step10/App.java
@@ -1,4 +1,4 @@
-package bitcamp.test.step09;
+package bitcamp.test.step10;
 
 // 1) 낱개의 변수 사용
 // 2) 낱개의 변수 재사용
@@ -9,6 +9,8 @@ package bitcamp.test.step09;
 // 7) GRASP 패턴: Information Expert(정보를 갖고 있는 클래스가 그 정보를 다룬다.)
 // 8) 인스턴스 메서드 도입
 // 9) 객체 생성이 복잡한 경우 메서드로 분리하는 것이 낫다.(디자인패턴; 팩터링 메서드)
+// 10) GRASP 패턴: Information Expert
+//    - createScore() 를 Score 클래스로 이동
 
 public class App {
   
@@ -24,25 +26,9 @@ public class App {
     this.sum = this.kor + this.eng + this.math;
     this.aver = this.sum / 3f;
    }
-  }
-  
-  public static void main(String[] args) {
-
-    final int MAX_SIZE = 10;
-    Score[] scores = new Score[MAX_SIZE];
-    int length = 0;
-
-    scores[length++] = createScore("홍길동", 100, 100, 100);
-    scores[length++] = createScore("임꺽정", 90, 90, 90);
-    scores[length++] = createScore("유관순", 80, 80, 80);
-
-    for (int i = 0; i < length; i++) {
-      printScore(scores[i]);
-    }
-  }
 
   // 팩토리 메서드
-  static Score createScore(String name, int kor, int eng, int math) {
+  static Score create(String name, int kor, int eng, int math) {
     Score s = new Score();
     s.name = name;
     s.kor = kor;
@@ -51,10 +37,25 @@ public class App {
     s.compute();
     return s;
   }
+  }
+  
+  public static void main(String[] args) {
+
+    final int MAX_SIZE = 10;
+    Score[] scores = new Score[MAX_SIZE];
+    int length = 0;
+
+    scores[length++] = Score.create("홍길동", 100, 100, 100);
+    scores[length++] = Score.create("임꺽정", 90, 90, 90);
+    scores[length++] = Score.create("유관순", 80, 80, 80);
+
+    for (int i = 0; i < length; i++) {
+      printScore(scores[i]);
+    }
+  }
 
   static void printScore(Score s) {
     System.out.printf("%s: 합계=%d, 평균=%.1f\n", 
       s.name, s.sum, s.aver);
   }
-
 }
