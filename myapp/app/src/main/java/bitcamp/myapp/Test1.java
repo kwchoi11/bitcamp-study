3d3a6a3e5e3ad40219260f901ad4e9ca3e57afd8
kwchoi11 <kwchoi0216@gmail.com>
2023-06-09 10:12:20 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/Test1.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test1.java
@@ -9,29 +9,30 @@ public class Test1 {
     // 2 * 3 + 7 - 2 / 2 = ?
     // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
 
-    int result = 0;
     
     result = multiple(2, 3);
     result = plus(result, 7);
     result = minus(result, 2);
     result = divide(result, 2);
-
+    
     System.out.println(result);
   }
+  
+  int result = 0;
 
-  static int plus(int a, int b) {
-    return a + b;
+  static void plus(int a) {
+    result += a;
   }
 
-  static int minus(int a, int b) {
-    return a - b;
+  static void minus(int a) {
+    result -= a;
   }
 
-  static int multiple(int a, int b) {
-    return a * b;
+  static int multiple(int a) {
+    result *= a;
   }
 
-  static int divide(int a, int b) {
-    return a / b;
+  static void divide(int a) {
+    result /= a;
   }
 }
