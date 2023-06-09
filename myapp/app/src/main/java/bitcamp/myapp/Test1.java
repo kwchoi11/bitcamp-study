09b312c27560d61415e4cc1c8bcc391955ddcfb8
kwchoi11 <kwchoi0216@gmail.com>
2023-06-09 10:09:00 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/Test1.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test1.java
@@ -1,7 +1,7 @@
 package bitcamp.myapp;
 
 // 소스 코드에서 Calculator 클래스는 bitcamp.util 패키지에 소속된 클래스를 가리킨다.
-import bitcamp.util.Calculator;
+
 
 public class Test1 {
 
@@ -9,11 +9,29 @@ public class Test1 {
     // 2 * 3 + 7 - 2 / 2 = ?
     // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
 
-    Calculator.init(2);
-    Calculator.multiple(3);
-    Calculator.plus(7);
-    Calculator.minus(2);
-    Calculator.divide(2);
-    System.out.println(Calculator.result);
+    int result = 0;
+    
+    result = multiple(2, 3);
+    result = plus(result, 7);
+    result = minus(result, 2);
+    result = divide(result, 2);
+
+    System.out.println(result);
+  }
+
+  static int plus(int a, int b) {
+    return a + b;
+  }
+
+  static int minus(int a, int b) {
+    return a - b;
+  }
+
+  static int multiple(int a, int b) {
+    return a * b;
+  }
+
+  static int divide(int a, int b) {
+    return a / b;
   }
 }
