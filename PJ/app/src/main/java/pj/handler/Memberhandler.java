2214da7b0839586573c2a9cfc0b0c76f494a8923
kwchoi11 <kwchoi0216@gmail.com>
2023-06-12 17:52:25 +0900

    .

--- PJ/app/src/main/java/pj/handler/Memberhandler.java
+++ PJ/app/src/main/java/pj/handler/Memberhandler.java
@@ -4,16 +4,12 @@ import pj.util.Prompt;
 import pj.vo.Member;
 
 public class Memberhandler {
-  
+
   static final int MAX_SIZE = 100;
   static Member[] members = new Member[MAX_SIZE];
-  static int userId = 1;
+  // static int userId = 1;
   static int length = 0;
 
-  static final char LEFT = 'L';
-  static final char RIGHT = 'W';
-  static final char PITCHER = 'P';
-  static final char BATTER = 'B';
 
   public static void inputMember() {
     if (!available()) {
@@ -22,13 +18,15 @@ public class Memberhandler {
     }
 
     Member m = new Member();
-    m.name = Prompt.inputString("선수 이름을 입력하세요.\n");
-    m.position = inputPosition((char)0);
-    m.strikeOuts = Prompt.inputString("선수의 탈삼진을 입력하세요. (투수)\n");
-    m.homeRuns = Prompt.inputString("선수의 홈런 수를 입력하세요. (타자)\n");
-    m.hand = inputHand((char)0);
-    m.no = userId++;
+    m.setName(Prompt.inputString("선수 이름을 입력하세요.\n"));
+    m.setPosition(inputPosition((char) 0));
+    m.setStrikeOuts(Prompt.inputString("선수의 탈삼진을 입력하세요. (투수)\n"));
+    m.setHomeRuns(Prompt.inputString("선수의 홈런 수를 입력하세요. (타자)\n"));
+    m.setHand(inputHand((char) 0));
+    // m.no = userId++;
 
+    // 위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게
+    // 레퍼런스 배열에 담는다.
     members[length++] = m;
   }
 
@@ -39,11 +37,9 @@ public class Memberhandler {
 
     for (int i = 0; i < length; i++) {
       Member m = members[i];
-      System.out.printf("%d, %s, %s, %s, %s, %s\n", 
-      m.no, m.name, 
-      toPositionString(m.position),
-      m.strikeOuts, m.homeRuns, 
-      toHandString(m.hand));
+      System.out.printf("%d, %s, %s, %s, %s, %s\n", m.getNo(), m.getName(),
+          toPositionString(m.getPosition()), m.getStrikeOuts(), m.getHomeRuns(),
+          toHandString(m.getHand()));
     }
   }
 
@@ -51,12 +47,12 @@ public class Memberhandler {
     String memberNo = Prompt.inputString("등록 번호를 입력해주세요.");
     for (int i = 0; i < length; i++) {
       Member m = members[i];
-      if (m.no == Integer.parseInt(memberNo)) {
-        System.out.printf("이름: %s\n", m.name);
-        System.out.printf("포지션: %s\n", toPositionString(m.position));
-        System.out.printf("탈삼진(투수): %s\n", m.strikeOuts);
-        System.out.printf("홈런 수(타자): %s\n", m.homeRuns);
-        System.out.printf("주 사용 손: %s\n", toHandString(m.hand));
+      if (m.getNo() == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", m.getName());
+        System.out.printf("포지션: %s\n", toPositionString(m.getPosition()));
+        System.out.printf("탈삼진(투수): %s\n", m.getStrikeOuts());
+        System.out.printf("홈런 수(타자): %s\n", m.getHomeRuns());
+        System.out.printf("주 사용 손: %s\n", toHandString(m.getHand()));
         return;
       }
     }
@@ -75,21 +71,21 @@ public class Memberhandler {
     String memberNo = Prompt.inputString("등록 번호를 입력해주세요.");
     for (int i = 0; i < length; i++) {
       Member m = members[i];
-      if (m.no == Integer.parseInt(memberNo)) {
-        System.out.printf("이름(%s): ", m.name);
-        m.name = Prompt.inputString("");
-        m.position = inputPosition(m.position);
-        System.out.printf("탈삼진(투수)(%s): ", m.strikeOuts);
-        m.strikeOuts = Prompt.inputString("");
-        System.out.printf("홈런 수(타자)(%s): ", m.homeRuns);
-        m.homeRuns = Prompt.inputString("");
-        m.hand = inputHand(m.hand);
+      if (m.getNo() == Integer.parseInt(memberNo)) {
+        System.out.printf("이름(%s): ", m.getName());
+        m.setName(Prompt.inputString(""));
+        m.setPosition(inputPosition(m.getPosition()));
+        System.out.printf("탈삼진(투수)(%s): ", m.getStrikeOuts());
+        m.setStrikeOuts(Prompt.inputString(""));
+        System.out.printf("홈런 수(타자)(%s): ", m.getHomeRuns());
+        m.setHomeRuns(Prompt.inputString(""));
+        m.setHand(inputHand(m.getHand()));
         return;
       }
     }
     System.out.println("해당 번호의 선수가 없습니다.");
   }
-  
+
 
   private static char inputPosition(char position) {
     String label;
@@ -98,17 +94,14 @@ public class Memberhandler {
     } else {
       label = String.format("포지션을 입력하세요(%s).\n", toPositionString(position));
     }
-    loop: while (true) {
-      String menuNo = Prompt.inputString(label +
-      " 1. 투수\n" +
-      " 2. 타자\n" +
-      "> ");
+    while (true) {
+      String menuNo = Prompt.inputString(label + " 1. 투수\n" + " 2. 타자\n" + "> ");
 
       switch (menuNo) {
         case "1":
-          return PITCHER;
+          return Member.PITCHER;
         case "2":
-          return BATTER;
+          return Member.BATTER;
         default:
           System.out.println("무효한 번호입니다.");
       }
@@ -124,10 +117,7 @@ public class Memberhandler {
       label = String.format("좌완(타)/우완(타)(%s)\n", toHandString(hand));
     }
     loop: while (true) {
-      String menuNo = Prompt.inputString(label +
-      " 1. 좌완(타)\n" +
-      " 2. 우완(타)\n" +
-      "> ");
+      String menuNo = Prompt.inputString(label + " 1. 좌완(타)\n" + " 2. 우완(타)\n" + "> ");
 
       switch (menuNo) {
         case "1":
