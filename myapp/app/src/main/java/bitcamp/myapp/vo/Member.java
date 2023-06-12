64e2e63e1dea90173b50fd6e8984621de65c1240
kwchoi11 <kwchoi0216@gmail.com>
2023-06-12 16:03:56 +0900

    private, setter/getter 추가

--- myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
@@ -8,20 +8,45 @@ public class Member {
   private char gender;
 
   public int getNo() {
-    return this.no;
+    return no;
   }
 
   public void setNo(int no) {
     this.no = no;
   }
 
-  public String getname() {
-    return this.name;
+  public String getName() {
+    return name;
   }
 
   public void setName(String name) {
     this.name = name;
   }
 
-  
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public char getGender() {
+    return gender;
+  }
+
+  public void setGender(char gender) {
+    this.gender = gender;
+  }
+
+
+
 }
