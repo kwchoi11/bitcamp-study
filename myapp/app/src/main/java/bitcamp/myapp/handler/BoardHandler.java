f3b28323121185e54407569f92806da0aa7ce9db
kwchoi11 <kwchoi0216@gmail.com>
2023-06-14 10:38:47 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
@@ -32,68 +32,64 @@ public class BoardHandler {
     for (int i = 0; i < length; i++) {
       Board board = boards[i];
 
-      System.out.printf("%d, %s, %s, %d, %tY-%5$tm-%5$td\n",
-          board.getNo(),
-          board.getTitle(),
-          board.getWriter(),
-          board.getViewCount(),
-          board.getCreatedDate());
+      System.out.printf("%d, %s, %s, %d, %tY-%5$tm-%5$td\n", board.getNo(), board.getTitle(),
+          board.getWriter(), board.getViewCount(), board.getCreatedDate());
     }
   }
 
   public static void viewBoard() {
-    //    String memberNo = Prompt.inputString("번호? ");
-    //    for (int i = 0; i < length; i++) {
-    //      Board m = boards[i];
-    //      if (m.getNo() == Integer.parseInt(memberNo)) {
-    //        System.out.printf("이름: %s\n", m.getName());
-    //        System.out.printf("이메일: %s\n", m.getEmail());
-    //        System.out.printf("성별: %s\n", toGenderString(m.getGender()));
-    //        return;
-    //      }
-    //    }
-    //    System.out.println("해당 번호의 회원이 없습니다!");
+    // String boardNo = Prompt.inputString("번호? ");
+    // for (int i = 0; i < length; i++) {
+    // Board = boards[i];
+    // if (m.getNo() == Integer.parseInt(memberNo)) {
+    // System.out.printf("이름: %s\n", m.getName());
+    // System.out.printf("이메일: %s\n", m.getEmail());
+    // System.out.printf("성별: %s\n", toGenderString(m.getGender()));
+    // return;
+    // }
+    // }
+    // System.out.println("해당 번호의 회원이 없습니다!");
   }
 
   public static void updateBoard() {
-    //    String memberNo = Prompt.inputString("번호? ");
-    //    for (int i = 0; i < length; i++) {
-    //      Board m = boards[i];
-    //      if (m.getNo() == Integer.parseInt(memberNo)) {
-    //        System.out.printf("이름(%s)? ", m.getName());
-    //        m.setName(Prompt.inputString(""));
-    //        System.out.printf("이메일(%s)? ", m.getEmail());
-    //        m.setEmail(Prompt.inputString(""));
-    //        System.out.printf("새암호? ");
-    //        m.setPassword(Prompt.inputString(""));
-    //        m.setGender(inputGender(m.getGender()));
-    //        return;
-    //      }
-    //    }
-    //    System.out.println("해당 번호의 회원이 없습니다!");
+    // String boardNo = Prompt.inputString("번호? ");
+    // for (int i = 0; i < length; i++) {
+    // Board board = boards[i];
+    // if (m.getNo() == Integer.parseInt(memberNo)) {
+    // System.out.printf("새암호? ");
+    // m.setPassword(Prompt.inputString(""));
+    // System.out.printf("이름(%s)? ", m.getName());
+    // m.setName(Prompt.inputString(""));
+    // System.out.printf("이메일(%s)? ", m.getEmail());
+    // m.setEmail(Prompt.inputString(""));
+    // m.setGender(inputGender(m.getGender()));
+    // return;
+    // }
+    // }
+    // System.out.println("해당 번호의 회원이 없습니다!");
   }
 
 
   public static void deleteBoard() {
-    //    int memberNo = Prompt.inputInt("번호? ");
-    //
-    //    int deletedIndex = indexOf(memberNo);
-    //    if (deletedIndex == -1) {
-    //      System.out.println("해당 번호의 회원이 없습니다!");
-    //      return;
-    //    }
-    //
-    //    for (int i = deletedIndex; i < length - 1; i++) {
-    //      boards[i] = boards[i + 1];
-    //    }
-    //
-    //    boards[--length] = null;
+    int boardNo = Prompt.inputInt("번호? ");
+
+    int deletedIndex = indexOf(boardNo);
+    if (deletedIndex == -1) {
+      System.out.println("해당 번호의 게시글이 없습니다!");
+      return;
+    }
+
+    for (int i = deletedIndex; i < length - 1; i++) {
+      boards[i] = boards[i + 1];
+    }
+
+    boards[--length] = null;
   }
 
-  private static int indexOf(int memberNo) {
+  private static int indexOf(int boardNo) {
     for (int i = 0; i < length; i++) {
-      Board m = boards[i];
-      if (m.getNo() == memberNo) {
+      Board board = boards[i];
+      if (board.getNo() == boardNo) {
         return i;
       }
     }
