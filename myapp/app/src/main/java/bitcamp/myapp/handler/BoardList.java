0c07f209363d9424a06b1ec3e164494d9e9a7d2f
kwchoi11 <kwchoi0216@gmail.com>
2023-06-15 15:28:11 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/BoardList.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/BoardList.java
@@ -4,6 +4,29 @@ import bitcamp.myapp.vo.Board;
 
 public class BoardList {
   private static final int MAX_SIZE = 100;
+
   private Board[] boards = new Board[MAX_SIZE];
   private int length = 0;
+
+  public boolean add(Board board) {
+    if (!this.available()) {
+      return false;
+    }
+    this.boards[this.length++] = board;
+    return true;
+  }
+
+  public Board[] list() {
+    Board[] arr = new Board[this.length];
+
+    for (int i = 0; i < this.length; i++) {
+      arr[i] = boards[i];
+    }
+
+    return arr;
+  }
+
+  private boolean available() {
+    return this.length < MAX_SIZE;
+  }
 }
