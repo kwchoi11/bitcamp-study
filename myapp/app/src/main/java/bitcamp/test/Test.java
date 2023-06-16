7c699049aeb95143139aabbed45952b19c83c2c8
kwchoi11 <kwchoi0216@gmail.com>
2023-06-16 12:23:47 +0900

    .

--- myapp/app/src/main/java/bitcamp/test/Test.java
+++ myapp/app/src/main/java/bitcamp/test/Test.java
@@ -2,10 +2,12 @@ package bitcamp.test;
 
 public class Test {
   public static void main(String[] args) {
-    Calculator2 c2 = new Calculator2();
+    Calculator c = new Calculator2();
+
+    Calculator2 c2 = (Calculator2) c;
 
     System.out.println(c2.minus(100, 200));
-    System.out.println(c2.plus(100, 200));
+    System.out.println(c.plus(100, 200));
 
   }
 
