2c440a6457f0b62bac217f251c35861a5c671a89
kwchoi11 <kwchoi0216@gmail.com>
2023-06-21 16:26:03 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/BoardDetailListener.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/BoardDetailListener.java
@@ -1,16 +1,13 @@
 package bitcamp.myapp.handler;
 
 import bitcamp.myapp.vo.Board;
-import bitcamp.util.ActionListener;
 import bitcamp.util.BreadcrumbPrompt;
 import bitcamp.util.List;
 
-public class BoardDetailListener implements ActionListener {
-
-  private List list;
+public class BoardDetailListener extends BoardListener {
 
   public BoardDetailListener(List list) {
-    this.list = list;
+    super(list);
   }
 
   @Override
