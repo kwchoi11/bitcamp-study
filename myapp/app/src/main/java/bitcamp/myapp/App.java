6f70023a7dd13ae7a6c27c7bab0ba6631e646955
kwchoi11 <kwchoi0216@gmail.com>
2023-06-21 15:12:17 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -5,8 +5,6 @@ import bitcamp.myapp.handler.BoardDeleteListener;
 import bitcamp.myapp.handler.BoardDetailListener;
 import bitcamp.myapp.handler.BoardListListener;
 import bitcamp.myapp.handler.BoardUpdateListener;
-import bitcamp.myapp.handler.Handler;
-import bitcamp.myapp.handler.MemberHandler;
 import bitcamp.util.ArrayList;
 import bitcamp.util.BreadcrumbPrompt;
 import bitcamp.util.LinkedList;
@@ -17,15 +15,16 @@ public class App {
 
   public static void main(String[] args) {
 
+    ArrayList memberList = new ArrayList();
     LinkedList boardList = new LinkedList();
+    LinkedList readingList = new LinkedList();
 
     BreadcrumbPrompt prompt = new BreadcrumbPrompt();
-
 
     MenuGroup mainMenu = new MenuGroup("메인");
 
     MenuGroup memberMenu = new MenuGroup("회원");
-    memberMenu.add(new Menu("등록"));
+    memberMenu.add(new Menu("등록", new BoardAddListener(memberList)));
     memberMenu.add(new Menu("목록"));
     memberMenu.add(new Menu("조회"));
     memberMenu.add(new Menu("변경"));
@@ -41,17 +40,17 @@ public class App {
     mainMenu.add(boardMenu);
 
     MenuGroup readingMenu = new MenuGroup("독서록");
-
-    readingMenu.add(new Menu("등록"));
-    readingMenu.add(new Menu("목록"));
-    readingMenu.add(new Menu("조회"));
-    readingMenu.add(new Menu("변경"));
-    readingMenu.add(new Menu("삭제"));
+    readingMenu.add(new Menu("등록", new BoardAddListener(readingList)));
+    readingMenu.add(new Menu("목록", new BoardListListener(readingList)));
+    readingMenu.add(new Menu("조회", new BoardDetailListener(readingList)));
+    readingMenu.add(new Menu("변경", new BoardUpdateListener(readingList)));
+    readingMenu.add(new Menu("삭제", new BoardDeleteListener(readingList)));
     mainMenu.add(readingMenu);
 
-    Handler memberHandler = new MemberHandler(prompt, "회원", new ArrayList());
-    Handler boardHandler = new BoardDeleteListener(prompt, "게시글", new LinkedList());
-    Handler readingHandler = new BoardDeleteListener(prompt, "독서록", new LinkedList());
+    // Handler memberHandler = new MemberHandler(prompt, "회원", new ArrayList()); // 의존 객체를 생성자를 통해
+    // 주입
+    // Handler boardHandler = new BoardHandler(prompt, "게시글", new LinkedList());
+    // Handler readingHandler = new BoardHandler(prompt, "독서록", new LinkedList());
 
     printTitle();
 
@@ -65,13 +64,13 @@ public class App {
     menu.append("1. 회원\n");
     menu.append("2. 게시글\n");
     menu.append("3. 독서록\n");
-    menu.append("0. 종료\n");
+    menu.append("0. 종료");
+
     return menu.toString();
   }
 
   static void printTitle() {
     System.out.println("나의 목록 관리 시스템");
-    System.out.println("----------------------------------");
+    System.out.println("—————————————————");
   }
-
 }
