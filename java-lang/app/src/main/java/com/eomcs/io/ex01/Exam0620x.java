377a6aa552acaef61d7776dae6653e657227f8e2
kwchoi11 <kwchoi0216@gmail.com>
2023-06-27 11:18:13 +0900

    .

---
+++ java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0620x.java
@@ -0,0 +1,35 @@
+// 디렉토리에 들어있는 파일(디렉토리) 목록을 꺼낼 때 필터 적용하기 II
+package com.eomcs.io.ex01;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class Exam0620x {
+
+  public static void main(String[] args) throws Exception {
+
+    class JavaFilter implements FileFilter {
+      @Override
+      public boolean accept(File file) {
+
+        if (file.isFile() && file.getName().endsWith(".java"))
+          return true; // 조회 결과에 포함시켜라!
+        return false; // 조회 결과에서 제외하라!
+      }
+    }
+    File dir = new File(".");
+
+    JavaFilter javaFilter = new JavaFilter();
+
+    File[] files = dir.listFiles(javaFilter);
+
+    for (File file : files) {
+      System.out.printf("%s %12d %s\n", file.isDirectory() ? "d" : "-", file.length(),
+          file.getName());
+    }
+
+  }
+
+}
+
+
