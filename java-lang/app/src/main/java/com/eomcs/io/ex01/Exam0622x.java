3f6de54ac1ebbc4cc0c699a891615b0f5d0f93d0
kwchoi11 <kwchoi0216@gmail.com>
2023-06-27 11:43:56 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0622x.java
+++ java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0622x.java
@@ -4,15 +4,22 @@ package com.eomcs.io.ex01;
 import java.io.File;
 import java.io.FileFilter;
 
-public class Exam0621x {
+public class Exam0622x {
+
   public static void main(String[] args) throws Exception {
-    FileFilter javaFilter = new FileFilter() {
+
+    class JavaFilter implements FileFilter {
       @Override
       public boolean accept(File file) {
-        return file.isFile() && file.getName().endsWith(".java");
+
+        if (file.isFile() && file.getName().endsWith(".java"))
+          return true; // 조회 결과에 포함시켜라!
+        return false; // 조회 결과에서 제외하라!
       }
-    };
+    }
     File dir = new File(".");
+
+    JavaFilter javaFilter = new JavaFilter();
 
     File[] files = dir.listFiles(javaFilter);
 
@@ -20,7 +27,9 @@ public class Exam0621x {
       System.out.printf("%s %12d %s\n", file.isDirectory() ? "d" : "-", file.length(),
           file.getName());
     }
+
   }
+
 }
 
 
