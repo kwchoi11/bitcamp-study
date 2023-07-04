4c972d6358d7b07bbb91b34ce6419ef582836b09
kwchoi11 <kwchoi0216@gmail.com>
2023-07-04 12:03:36 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/oop/ex09/b/Exam04.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex09/b/Exam04.java
@@ -20,23 +20,24 @@ interface MyInterface4 {
   // 구현 메서드를 정의할 수 있다.
   // 언제?
   // - 해당 기능을 m2()와 m3() 처럼 여러 메서드에서 사용해야 할 경우
-  //   그 공통 코드를 다음과 같이 private 구현 메서드로 정의하면 될 것이다.
+  // 그 공통 코드를 다음과 같이 private 구현 메서드로 정의하면 될 것이다.
   //
   private void x() {
     System.out.println("MyInterface4.x()");
   }
 }
 
+
 // 2) 인터페이스 구현
 class MyInterface4Impl implements MyInterface4 {
   @Override
   public void m1() {
 
-    //    m2(); // 현재 클래스의 m2() 메서드
+    // m2(); // 현재 클래스의 m2() 메서드
 
     // 인터페이스에 선언된 오버라이딩 전의 default 메서드를 호출하고 싶다면,
-    //    super.m2(); // 수퍼 클래스(Object)에서 m2()를 찾아 올라간다. Object에는 m2()가 없기 때문에 컴파일 오류!
-    //    MyInterface4.m2(); // m2()는 인스턴스(non-static) 메서드이기 때문에 인터페이스 이름으로 직접 호출 불가!
+    // super.m2(); // 수퍼 클래스(Object)에서 m2()를 찾아 올라간다. Object에는 m2()가 없기 때문에 컴파일 오류!
+    // MyInterface4.m2(); // m2()는 인스턴스(non-static) 메서드이기 때문에 인터페이스 이름으로 직접 호출 불가!
     MyInterface4.super.m2();
 
     System.out.println("MyInterface4Impl.m1()");
@@ -47,6 +48,7 @@ class MyInterface4Impl implements MyInterface4 {
     System.out.println("MyInterface4Impl.m2()");
   }
 }
+
 
 public class Exam04 {
 
@@ -63,7 +65,7 @@ public class Exam04 {
     System.out.println("-----------------------------");
 
     // 인터페이스에 정의된 private 메서드는 호출할 수 없다.
-    //    obj.x(); // 컴파일 오류!
+    // obj.x(); // 컴파일 오류!
 
   }
 
