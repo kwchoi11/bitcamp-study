4dcf7f2d868da5f50cde54e136fdb81bb6245a34
kwchoi11 <kwchoi0216@gmail.com>
2023-07-05 14:19:56 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/dao/BoardListDao.java
+++ myapp/app/src/main/java/bitcamp/myapp/dao/BoardListDao.java
@@ -1,17 +1,9 @@
 package bitcamp.myapp.dao;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import bitcamp.myapp.vo.AutoIncrement;
 import bitcamp.myapp.vo.Board;
+import bitcamp.util.JsonDataHelper;
 
 public class BoardListDao implements BoardDao {
 
@@ -20,15 +12,15 @@ public class BoardListDao implements BoardDao {
 
   public BoardListDao(String filename) {
     this.filename = filename;
-    loadJson(list, Board.class);
+    JsonDataHelper.loadJson(filename, list, Board.class);
   }
 
   @Override
   public void insert(Board board) {
     board.setNo(Board.boardNo++);
+    board.setCreatedDate(System.currentTimeMillis());
     this.list.add(board);
-
-    saveJson(list);
+    JsonDataHelper.saveJson(filename, list);
   }
 
   @Override
@@ -39,14 +31,24 @@ public class BoardListDao implements BoardDao {
   @Override
   public Board findBy(int no) {
     for (int i = 0; i < this.list.size(); i++) {
-      Board board = this.list.get(i);
-      if (board.getNo() == no)
-        ;
-      {
-        return board;
+      Board m = this.list.get(i);
+      if (m.getNo() == no) {
+        return m;
       }
     }
     return null;
+  }
+
+  @Override
+  public int update(Board board) {
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).getNo() == board.getNo()) {
+        list.set(i, board);
+        JsonDataHelper.saveJson(filename, list);
+        return 1;
+      }
+    }
+    return 0;
   }
 
   @Override
@@ -54,58 +56,10 @@ public class BoardListDao implements BoardDao {
     for (int i = 0; i < list.size(); i++) {
       if (list.get(i).getNo() == no) {
         list.remove(i);
+        JsonDataHelper.saveJson(filename, list);
         return 1;
       }
     }
     return 0;
-  }
-
-  private <T> void loadJson(String filename, List<T> list, Class<T> clazz) {
-    try {
-      FileReader in0 = new FileReader(filename);
-      BufferedReader in = new BufferedReader(in0); // <== Decorator 역할을 수행!
-
-      StringBuilder strBuilder = new StringBuilder();
-      String line = null;
-
-      while ((line = in.readLine()) != null) {
-        strBuilder.append(line);
-      }
-
-      in.close();
-
-      Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
-      Collection<T> objects = gson.fromJson(strBuilder.toString(),
-          TypeToken.getParameterized(Collection.class, clazz).getType());
-
-      list.addAll(objects);
-
-      Class<?>[] interfaces = clazz.getInterfaces();
-      for (Class<?> info : interfaces) {
-        if (info == AutoIncrement.class) {
-          AutoIncrement autoIncrement = (AutoIncrement) list.get(list.size() - 1);
-          autoIncrement.updateKey();
-          break;
-        }
-      }
-
-    } catch (Exception e) {
-      System.out.println(filename + " 파일을 읽는 중 오류 발생!");
-    }
-  }
-
-  private void saveJson(String filename, List<?> list) {
-    try {
-      FileWriter out0 = new FileWriter(filename);
-      BufferedWriter out = new BufferedWriter(out0);
-
-      Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create();
-      out.write(gson.toJson(list));
-
-      out.close();
-
-    } catch (Exception e) {
-      System.out.println(filename + " 파일을 저장하는 중 오류 발생!");
-    }
   }
 }
