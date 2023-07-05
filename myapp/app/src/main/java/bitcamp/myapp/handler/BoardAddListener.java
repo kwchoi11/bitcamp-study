51ecda2e33ad19fd4539b9f443d392fe63ef8569
kwchoi11 <kwchoi0216@gmail.com>
2023-07-05 14:07:51 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/BoardAddListener.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/BoardAddListener.java
@@ -2,9 +2,10 @@ package bitcamp.myapp.handler;
 
 import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.Board;
+import bitcamp.util.ActionListener;
 import bitcamp.util.BreadcrumbPrompt;
 
-public class BoardAddListener implements BoardActionListener {
+public class BoardAddListener implements ActionListener {
 
   BoardDao boardDao;
 
@@ -15,12 +16,10 @@ public class BoardAddListener implements BoardActionListener {
   @Override
   public void service(BreadcrumbPrompt prompt) {
     Board board = new Board();
-    board.setNo(Board.boardNo++);
     board.setTitle(prompt.inputString("제목? "));
     board.setContent(prompt.inputString("내용? "));
     board.setWriter(prompt.inputString("작성자? "));
     board.setPassword(prompt.inputString("암호? "));
-    board.setCreatedDate(System.currentTimeMillis());
 
     boardDao.insert(board);
   }
