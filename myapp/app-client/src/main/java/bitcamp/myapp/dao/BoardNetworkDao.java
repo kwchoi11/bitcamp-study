e0c0ce5dea2fa43d00c76508d5898cabe202efff
kwchoi11 <kwchoi0216@gmail.com>
2023-07-06 11:03:07 +0900

    .

--- myapp/app-client/src/main/java/bitcamp/myapp/dao/BoardNetworkDao.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/dao/BoardNetworkDao.java
@@ -2,9 +2,11 @@ package bitcamp.myapp.dao;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import bitcamp.myapp.vo.Board;
 
@@ -24,19 +26,22 @@ public class BoardNetworkDao implements BoardDao {
   @Override
   public void insert(Board board) {
     try {
-      // 서버에서 실행할 명령을 보낸다.
-      out.writeUTF(dataName + "/insert");
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-      // 명령을 실행할 때 사용할 데이터를 보낸다.
-      Gson gson = new Gson();
-      String jsonStr = new Gson().toJson(board);
-      out.writeUTF(jsonStr);
+      // 서버에 보낼 명령과 데이터를 Map 객체에 담는다.
+      HashMap<String, Object> request = new HashMap<>();
+      request.put("command", dataName + "/insert");
+      request.put("data", board);
 
-      // 서버에서 보낸 응답을 받는다.
-      Map<String, Object> response = gson.fromJson(in.readUTF(), Map.class);
-      if (!response.get("status").equals("success")) {
-        throw new RuntimeException((String) response.get("message"));
-      }
+      System.out.println(gson.toJson(request));
+      // // Map 객체에 담은 정보를 JSON 문자열로 변환하여 서버에 보낸다.
+      // out.writeUTF(gson.toJson(request));
+      //
+      // // 서버에서 보낸 응답을 받는다.
+      // Map<String, Object> response = gson.fromJson(in.readUTF(), Map.class);
+      // if (!response.get("status").equals("success")) {
+      // throw new RuntimeException((String) response.get("message"));
+      // }
     } catch (Exception e) {
       System.out.println(e.getMessage());
       e.printStackTrace();
