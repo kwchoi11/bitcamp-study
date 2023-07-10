a5434c3e66e2d584480f4acc4745feebf04ac829
kwchoi11 <kwchoi0216@gmail.com>
2023-07-10 11:13:26 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -2,28 +2,35 @@ package bitcamp.myapp;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import bitcamp.myapp.dao.BoardDao;
+import java.util.HashMap;
 import bitcamp.myapp.dao.BoardListDao;
-import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.dao.MemberListDao;
-import bitcamp.myapp.vo.Board;
-import bitcamp.myapp.vo.Member;
 import bitcamp.net.RequestEntity;
 import bitcamp.net.ResponseEntity;
 
+// 1) 클라이언트가 보낸 명령을 데이터 이름과 메서드 이름으로 분리한다.
+// 2) 클라이언트가 요청한 DAO 객체와 메서드를 찾는다.
+// 3) 메서드의 파라미터와 리턴 타입 알아내기.
+// 4) 메서드 호출 및 리턴 값 받기
 public class ServerApp {
 
   int port;
   ServerSocket serverSocket;
 
-  MemberDao memberDao = new MemberListDao("member.json");
-  BoardDao boardDao = new BoardListDao("board.json");
-  BoardDao readingDao = new BoardListDao("reading.json");
+  // 클라이언트 요청을 처리할 DAO 객체를 맵에 보관한다.
+  HashMap<String, Object> daoMap = new HashMap<>();
 
   public ServerApp(int port) throws Exception {
     this.port = port;
+
+    // Dao 객체 생성 및 보관
+    daoMap.put("member", new MemberListDao("memberjson"));
+    daoMap.put("board", new BoardListDao("board.json"));
+    daoMap.put("reading", new BoardListDao("reading.json"));
   }
 
   public void close() throws Exception {
@@ -61,82 +68,63 @@ public class ServerApp {
         break;
       }
 
-      ResponseEntity response = new ResponseEntity();
+      // 데이터 이름과 메서드 이름 알아내기
+      String[] values = command.split("/");
+      String dataName = values[0];
+      String methodName = values[1];
 
-      switch (command) {
-        case "board/list":
-          response.status(ResponseEntity.SUCCESS).result(boardDao.list());
-          break;
-        case "board/insert":
-          boardDao.insert(request.getObject(Board.class));
-          response.status(ResponseEntity.SUCCESS);
-          break;
-        case "board/findBy":
-          Board board = boardDao.findBy(request.getObject(Integer.class));
-          if (board == null) {
-            response.status(ResponseEntity.SUCCESS);
-          } else {
-            response.status(ResponseEntity.SUCCESS).result(board);
-          }
-          break;
-        case "board/update":
-          int value = boardDao.update(request.getObject(Board.class));
-          response.status(ResponseEntity.SUCCESS).result(value);
-          break;
-        case "board/delete":
-          value = boardDao.delete(request.getObject(Integer.class));
-          response.status(ResponseEntity.SUCCESS).result(value);
-          break;
-        case "member/list":
-          response.status(ResponseEntity.SUCCESS).result(memberDao.list());
-          break;
-        case "member/insert":
-          memberDao.insert(request.getObject(Member.class));
-          response.status(ResponseEntity.SUCCESS);
-          break;
-        case "member/findBy":
-          Member member = memberDao.findBy(request.getObject(Integer.class));
-          if (member == null) {
-            response.status(ResponseEntity.SUCCESS);
-          } else {
-            response.status(ResponseEntity.SUCCESS).result(member);
-          }
-          break;
-        case "member/update":
-          value = memberDao.update(request.getObject(Member.class));
-          response.status(ResponseEntity.SUCCESS).result(value);
-          break;
-        case "member/delete":
-          value = memberDao.delete(request.getObject(Integer.class));
-          response.status(ResponseEntity.SUCCESS).result(value);
-          break;
-        case "reading/list":
-          response.status(ResponseEntity.SUCCESS).result(boardDao.list());
-          break;
-        case "reading/insert":
-          boardDao.insert(request.getObject(Board.class));
-          response.status(ResponseEntity.SUCCESS);
-          break;
-        case "reading/findBy":
-          board = boardDao.findBy(request.getObject(Integer.class));
-          if (board == null) {
-            response.status(ResponseEntity.SUCCESS);
-          } else {
-            response.status(ResponseEntity.SUCCESS).result(board);
-          }
-          break;
-        case "reading/update":
-          value = boardDao.update(request.getObject(Board.class));
-          response.status(ResponseEntity.SUCCESS).result(value);
-          break;
-        case "reading/delete":
-          value = boardDao.delete(request.getObject(Integer.class));
-          response.status(ResponseEntity.SUCCESS).result(value);
-          break;
-        default:
-          response.status(ResponseEntity.ERROR).result("해당 명령을 지원하지 않습니다!");
+      // 데이터 이름으로 DAO 객체를 꺼낸다.
+      Object dao = daoMap.get(dataName);
+      if (dao == null) {
+        // 만약 데이터를 처리할 DAO를 찾지 못 한다면 오류 정보를 클라이언트에게 보낸다.
+        out.writeUTF(
+            new ResponseEntity().status(ResponseEntity.ERROR).result("데이터를 찾을 수 없습니다.").toJson());
+        continue;
       }
 
+      // DAO에 해당 메서드가 있는지 알아낸다.
+      Method[] methods = dao.getClass().getDeclaredMethods();
+      Method method = null;
+      for (int i = 0; i < methods.length; i++) {
+        if (methods[i].getName().equals(methodName)) {
+          method = methods[i];
+          break;
+        }
+      }
+
+      if (method == null) {
+        // 만약 클라이언트가 요청한 메서드를 찾지 못 한다면, 오류 정보를 클라이언트에게 보낸다.
+        out.writeUTF(
+            new ResponseEntity().status(ResponseEntity.ERROR).result("메서드를 찾을 수 없습니다.").toJson());
+        continue;
+      }
+
+      // System.out.printf("%s.%s\n", dataName, methodName);
+
+      // 메서드의 파라미터와 리턴 타입 알아내기.
+      Parameter[] params = method.getParameters();
+
+      // if (params.length > 0) {
+      // System.out.println(params[0].getType().getSimpleName());
+      // }
+      // System.out.println(returnType.getSimpleName());
+
+      // 메서드 호출
+      Object returnValue = null;
+
+      // => 호출할 메서드가 파라미터를 가지고 있다면,
+      if (params.length > 0) {
+        // => 클라이언트가 보낸 JSON 데이터를 메서드의 파라미터 값으로 deserialize 한다.
+        Object arg = request.getObject(params[0].getType());
+        returnValue = method.invoke(dao, arg);
+      } else {
+        returnValue = method.invoke(dao);
+      }
+
+      // 메서드 호출 결과를 클라이언트에게 보낸다.
+      ResponseEntity response = new ResponseEntity();
+      response.status(ResponseEntity.SUCCESS);
+      response.result(returnValue);
       out.writeUTF(response.toJson());
     }
 
