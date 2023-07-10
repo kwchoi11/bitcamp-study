7ebb7ad3f4199913c9a25a2fbcc2e1afd08818be
kwchoi11 <kwchoi0216@gmail.com>
2023-07-10 16:03:51 +0900

    .

--- PJ/app/review/app-02/main/java/pj/App.java
+++ PJ/app/review/app-02/main/java/pj/App.java
@@ -2,21 +2,21 @@ package pj;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("나의 목록 관리 시스템");
+    System.out.println("SSG 랜더스 선수 관리 시스템");
     System.out.println("---------------------");
 
-    System.out.print("번호: ");
-    System.out.println(100);
+    System.out.print("등번호: ");
+    System.out.println(29);
 
-    System.out.printf("이름: %s", "홍길동");
+    System.out.printf("이름: %s", "김광현");
     System.out.println();
 
-    System.out.println("나이: " + 20);
+    System.out.println("나이: " + 34);
 
-    System.out.printf("재직자: %b\n", true);
+    System.out.printf("포지션: %s\n", "투수");
 
-    System.out.printf("성별(남자(M), 여자(W)): %c\n", 'M');
+    System.out.println("탈삼진: " + 58);
 
-    System.out.printf("좌우시력: %.1f, %.1f\n", 1.5f, 1.0f);
+    System.out.printf("투타: %s\n", "좌투좌타");
   }
 }
