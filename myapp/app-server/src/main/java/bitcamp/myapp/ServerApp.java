d8d501d8d90af6648a4cb2058f54e1757d16f816
kwchoi11 <kwchoi0216@gmail.com>
2023-07-11 10:21:16 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -53,62 +53,11 @@ public class ServerApp {
     this.serverSocket = new ServerSocket(port);
     System.out.println("서버 실행 중...");
 
-    Socket socket = serverSocket.accept();
-    DataInputStream in = new DataInputStream(socket.getInputStream());
-    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
     while (true) {
-      RequestEntity request = RequestEntity.fromJson(in.readUTF());
-
-      String command = request.getCommand();
-      System.out.println(command);
-
-      if (command.equals("quit")) {
-        break;
-      }
-
-      String[] values = command.split("/");
-      String dataName = values[0];
-      String methodName = values[1];
-
-      Object dao = daoMap.get(dataName);
-      if (dao == null) {
-        out.writeUTF(
-            new ResponseEntity().status(ResponseEntity.ERROR).result("데이터를 찾을 수 없습니다.").toJson());
-        continue;
-      }
-
-      // DAO 객체에서 메서드 찾기
-      Method method = findMethod(dao, methodName);
-      if (method == null) {
-        out.writeUTF(
-            new ResponseEntity().status(ResponseEntity.ERROR).result("메서드를 찾을 수 없습니다.").toJson());
-        continue;
-      }
-
-      try {
-        // DAO 메서드 호출하기
-        Object result = call(dao, method, request);
-
-        ResponseEntity response = new ResponseEntity();
-        response.status(ResponseEntity.SUCCESS);
-        response.result(result);
-        out.writeUTF(response.toJson());
-
-      } catch (Exception e) {
-        ResponseEntity response = new ResponseEntity();
-        response.status(ResponseEntity.ERROR);
-        response.result(e.getMessage());
-        out.writeUTF(response.toJson());
-      }
+      processRequest(serverSocket.accept());
     }
-
-    in.close();
-    out.close();
-    socket.close();
   }
 
-  // 메서드 찾기
   public static Method findMethod(Object obj, String methodName) {
     Method[] methods = obj.getClass().getDeclaredMethods();
     for (int i = 0; i < methods.length; i++) {
@@ -119,13 +68,69 @@ public class ServerApp {
     return null;
   }
 
-  // 메서드 호출하기
   public static Object call(Object obj, Method method, RequestEntity request) throws Exception {
     Parameter[] params = method.getParameters();
     if (params.length > 0) {
       return method.invoke(obj, request.getObject(params[0].getType()));
     } else {
       return method.invoke(obj);
+    }
+  }
+
+  // 클라이언트와 접속이 이루어지면 클라이언트의 요청을 처리한다.
+  public void processRequest(Socket socket) {
+    try (Socket s = socket;
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+
+      while (true) {
+        RequestEntity request = RequestEntity.fromJson(in.readUTF());
+
+        String command = request.getCommand();
+        System.out.println(command);
+
+        if (command.equals("quit")) {
+          break;
+        }
+
+        String[] values = command.split("/");
+        String dataName = values[0];
+        String methodName = values[1];
+
+        Object dao = daoMap.get(dataName);
+        if (dao == null) {
+          out.writeUTF(
+              new ResponseEntity().status(ResponseEntity.ERROR).result("데이터를 찾을 수 없습니다.").toJson());
+          continue;
+        }
+
+        // DAO 객체에서 메서드 찾기
+        Method method = findMethod(dao, methodName);
+        if (method == null) {
+          out.writeUTF(
+              new ResponseEntity().status(ResponseEntity.ERROR).result("메서드를 찾을 수 없습니다.").toJson());
+          continue;
+        }
+
+        try {
+          // DAO 메서드 호출하기
+          Object result = call(dao, method, request);
+
+          ResponseEntity response = new ResponseEntity();
+          response.status(ResponseEntity.SUCCESS);
+          response.result(result);
+          out.writeUTF(response.toJson());
+
+        } catch (Exception e) {
+          ResponseEntity response = new ResponseEntity();
+          response.status(ResponseEntity.ERROR);
+          response.result(e.getMessage());
+          out.writeUTF(response.toJson());
+        }
+      }
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
     }
   }
 }
