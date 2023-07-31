f5cd0090055a61f1ffd1de2860e12368dc700444
kwchoi11 <kwchoi0216@gmail.com>
2023-07-31 17:21:18 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/util/ApplicationContext.java
+++ myapp/app-server/src/main/java/bitcamp/util/ApplicationContext.java
@@ -67,14 +67,34 @@ public class ApplicationContext {
   }
 
   private void createBeans(String basePackage) throws Exception {
-    InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(basePackage.replaceAll("[.]", "/"));
-    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-    reader
-    .lines()
-    .filter(line -> {
-      System.out.println(line);
-      return false;
-    });
+    // 패키지 이름에 해당하는 디렉토리 정보를 알아낸다.
+    // 1) 패키지 이름을 파일 경로로 변환한다. (. --> /)
+    String packagePath = basePackage.replaceAll("[.]", "/");
+
+    // 2) 클래스 경로(CLASSPATH)를 관리하는 객체를 준비한다.
+    ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+
+    // 3) 클래스로더에게 패키지 경로에 해당하는 디렉토리 정보를 읽을 수 있는 도구를 달라고 요구한다.
+    InputStream dirInputStream = systemClassLoader.getResourceAsStream(packagePath);
+
+    // 4) 패키지 디렉토리 안에 들어있는 파일 이름이나 하위 디렉토리 이름을 읽을 도구를 준비한다.
+    BufferedReader dirReader = new BufferedReader(new InputStreamReader(dirInputStream));
+
+    // 5) 디렉토리 리더를 통해 해당 디렉토리에 들어있는 하위 디렉토리 또는 파일 이름을 알아낸다.
+    //    - 한 줄씩 읽으면 된다.
+    String line = null;
+    while ((line == dirReader.readLine()) != null) {
+      if (line.endsWith(".class")) {
+        System.out.println(line);
+      }
+    }
+
+    //    reader
+    //    .lines()
+    //    .filter(line -> {
+    //      System.out.println(line);
+    //      return false;
+    //    });
   }
 
   public Object getBean(String name) {
