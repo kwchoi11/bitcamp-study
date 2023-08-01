5e66af384964373c96cbc882642b6a73f12831db
kwchoi11 <kwchoi0216@gmail.com>
2023-08-01 15:16:39 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerHandler.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerHandler.java
@@ -1,27 +1,26 @@
 package bitcamp.myapp;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.HttpRequest;
 
 
-public class ServerHandler extends ChannelInboundHandlerAdapter {
+public class ServerHandler  extends SimpleChannelInboundHandler {
+  private HttpRequest request;
+  StringBuilder responseData = new StringBuilder();
 
   @Override
-  public void channelRead(ChannelHandlerContext ctx, Object msg) {
-    // 클라이언트가 요청을 보내면 이 메서드가 호출된다.
-    System.out.println("ServerHandler.channelRead() 호출됨!");
+  public void channelReadComplete(ChannelHandlerContext ctx) {
+    ctx.flush();
+  }
 
-    // 클라이언트로부터 받은 요청 데이터를 메모리에서 해제한다.
-    ((ByteBuf) msg).release();
+  @Override
+  protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
+    // implementation to follow
   }
 
   @Override
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-    // 클라이언트와 통신하는 중에 예외가 발생하면 이 메서드가 호출된다.
-    System.out.println("ServerHandler.exceptionCaught()");
-
-    // 예외에 대한 자세한 정보를 출력한다.
     cause.printStackTrace();
     ctx.close();
   }
