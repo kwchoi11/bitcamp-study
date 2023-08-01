fb519f86e39e9c939efefdd48d2997e0c7c39c08
kwchoi11 <kwchoi0216@gmail.com>
2023-08-01 10:40:40 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
@@ -9,14 +9,13 @@ import bitcamp.util.SqlSessionFactoryProxy;
 
 // Application을 실행하는데 필요한 객체를 설정하는 일을 한다.
 //
-@ComponentScan(basePackages={"bitcamp.myapp.dao","bitcamp.myapp.handler"})
+@ComponentScan(basePackages = {"bitcamp.myapp.dao"})
 public class AppConfig {
 
   // Mybatis 객체 준비
   @Bean
   public SqlSessionFactory sqlSessionFactory() throws Exception {
-    return new SqlSessionFactoryProxy(
-        new SqlSessionFactoryBuilder().build(
-            Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml")));
+    return new SqlSessionFactoryProxy(new SqlSessionFactoryBuilder()
+        .build(Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml")));
   }
 }
