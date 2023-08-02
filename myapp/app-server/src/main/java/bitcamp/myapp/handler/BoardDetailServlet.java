11ccd504936c1031cea9231a80add569e9f51e6f
kwchoi11 <kwchoi0216@gmail.com>
2023-08-02 14:43:08 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardDetailServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardDetailServlet.java
@@ -46,18 +46,18 @@ public class BoardDetailServlet implements Servlet {
       out.printf("<input type='hidden' name='category' value='%d'>\n", board.getCategory());
       out.println("<table border='1'>");
       out.printf("<tr><th style='width:120px;'>번호</th>"
-          + "<td style='width:300px;'><input type='text' name='no' value='%d' readonly='readonly'></td></tr>\n", board.getNo());
+          + " <td style='width:300px;'><input type='text' name='no' value='%d' readonly></td></tr>\n", board.getNo());
       out.printf("<tr><th>제목</th>"
-          + "<td><input type='text' name='title' value='%s'></td></tr>\n", board.getTitle());
+          + " <td><input type='text' name='title' value='%s'></td></tr>\n", board.getTitle());
       out.printf("<tr><th>내용</th>"
-          + "<td><textarea name='content' style='height:200px; width:400px;'>%s</textarea></td></tr>\n", board.getContent());
-      out.printf("<tr><th>작성자</th><td>%s</td></tr>\n", board.getWriter().getName());
-      out.printf("<tr><th>조회수</th><td>%s</td></tr>\n", board.getViewCount());
-      out.printf("<tr><th>등록일</th><td>%tY-%1$tm-%1$td</td></tr>\n", board.getCreatedDate());
+          + " <td><textarea name='content' style='height:200px; width:400px;'>%s</textarea></td></tr>\n", board.getContent());
+      out.printf("<tr><th>작성자</th> <td>%s</td></tr>\n", board.getWriter().getName());
+      out.printf("<tr><th>조회수</th> <td>%s</td></tr>\n", board.getViewCount());
+      out.printf("<tr><th>등록일</th> <td>%tY-%1$tm-%1$td</td></tr>\n", board.getCreatedDate());
       out.println("</table>");
 
       out.println("<div>");
-      out.println("<button>변경</botton>");
+      out.println("<button>변경</button>");
       out.println("<button type='reset'>초기화</button>");
       out.printf("<a href='/board/list?category=%d'>목록</a>\n", board.getCategory());
       out.println("</div>");
