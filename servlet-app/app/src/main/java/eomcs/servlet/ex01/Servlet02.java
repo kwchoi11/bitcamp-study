02e5efb5448d189c6b9f1a08da1f8998e9ec6a01
kwchoi11 <kwchoi0216@gmail.com>
2023-08-07 17:30:58 +0900

    2023-08-07 웹 프로그래밍 수업 HttpServlet

--- servlet-app/app/src/main/java/eomcs/servlet/ex01/Servlet02.java
+++ servlet-app/app/src/main/java/eomcs/servlet/ex01/Servlet02.java
@@ -5,6 +5,7 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 //GenericServlet 추상 클래스
 //=> javax.servlet.Servlet 인터페이스를 구현하였다.
@@ -24,6 +25,7 @@ import javax.servlet.ServletResponse;
 //@WebServlet(urlPatterns="/ex01/s2")
 //@WebServlet(value="/ex01/s2")
 
+@WebServlet("/ex01/s2")
 public class Servlet02 extends GenericServlet {
 
   // GenericServlet 추상 클래스는 java.io.Serialize 인터페이스를 구현하였다.
