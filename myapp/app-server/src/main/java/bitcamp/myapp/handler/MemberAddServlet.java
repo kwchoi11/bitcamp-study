9adc6543b835c1e0bb97d4da9615d0c71b644afd
kwchoi11 <kwchoi0216@gmail.com>
2023-08-23 16:00:26 +0900

    2023.8.23 myapp 쿠키과 ServletContext 보관소 - InitServlet 종속성 제거

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberAddServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberAddServlet.java
@@ -9,7 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.NcpObjectStorageService;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 @WebServlet("/member/add")
 @MultipartConfig(maxFileSize = 1024 * 1024 * 10)
@@ -21,6 +26,10 @@ public class MemberAddServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
 
+    MemberDao memberDao = (MemberDao) this.getServletContext().getAttribute("memberDao");
+    SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) this.getServletContext().getAttribute("sqlSessionFactory");
+    NcpObjectStorageService ncpObjectStorageService = (NcpObjectStorageService) this.getServletContext().getAttribute("ncpObjectStorageService");
+
     Member m = new Member();
     m.setName(request.getParameter("name"));
     m.setEmail(request.getParameter("email"));
@@ -29,7 +38,7 @@ public class MemberAddServlet extends HttpServlet {
 
     Part photoPart = request.getPart("photo");
     if (photoPart.getSize() > 0) {
-      String uploadFileUrl = InitServlet.ncpObjectStorageService.uploadFile(
+      String uploadFileUrl = ncpObjectStorageService.uploadFile(
               "bitcamp-nc7-bucket-04", "member/", photoPart);
       m.setPhoto(uploadFileUrl);
     }
@@ -48,12 +57,12 @@ public class MemberAddServlet extends HttpServlet {
     out.println("<h1>회원 등록</h1>");
 
     try {
-      InitServlet.memberDao.insert(m);
-      InitServlet.sqlSessionFactory.openSession(false).commit();
+      memberDao.insert(m);
+      sqlSessionFactory.openSession(false).commit();
       out.println("<p>등록 성공입니다!</p>");
 
     } catch (Exception e) {
-      InitServlet.sqlSessionFactory.openSession(false).rollback();
+      sqlSessionFactory.openSession(false).rollback();
       out.println("<p>등록 실패입니다!</p>");
       e.printStackTrace();
     }
--- myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberUpdateServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberUpdateServlet.java
@@ -9,7 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.NcpObjectStorageService;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 @WebServlet("/member/update")
 @MultipartConfig(maxFileSize = 1024 * 1024 * 10)
@@ -28,9 +33,13 @@ public class MemberUpdateServlet extends HttpServlet {
     member.setPassword(request.getParameter("password"));
     member.setGender(request.getParameter("gender").charAt(0));
 
+    MemberDao memberDao = (MemberDao) this.getServletContext().getAttribute("memberDao");
+    SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) this.getServletContext().getAttribute("sqlSessionFactory");
+    NcpObjectStorageService ncpObjectStorageService = (NcpObjectStorageService) this.getServletContext().getAttribute("ncpObjectStorageService");
+
     Part photoPart = request.getPart("photo");
     if (photoPart.getSize() > 0) {
-      String uploadFileUrl = InitServlet.ncpObjectStorageService.uploadFile(
+      String uploadFileUrl = ncpObjectStorageService.uploadFile(
               "bitcamp-nc7-bucket-04", "member/", photoPart);
       member.setPhoto(uploadFileUrl);
     }
@@ -48,14 +57,14 @@ public class MemberUpdateServlet extends HttpServlet {
     out.println("<h1>회원 변경</h1>");
 
     try {
-      if (InitServlet.memberDao.update(member) == 0) {
+      if (memberDao.update(member) == 0) {
         out.println("<p>회원이 없습니다.</p>");
       } else {
-        InitServlet.sqlSessionFactory.openSession(false).commit();
+        sqlSessionFactory.openSession(false).commit();
         out.println("<p>변경했습니다!</p>");
       }
     } catch (Exception e) {
-      InitServlet.sqlSessionFactory.openSession(false).rollback();
+      sqlSessionFactory.openSession(false).rollback();
       out.println("<p>변경 실패입니다!</p>");
       e.printStackTrace();
     }
