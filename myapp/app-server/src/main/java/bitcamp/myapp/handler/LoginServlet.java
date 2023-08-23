300152d4ed4c093b8f4d4549e3fb9eeb0ccab7c7
kwchoi11 <kwchoi0216@gmail.com>
2023-08-23 15:02:23 +0900

    2023.8.23 myapp 쿠키 적용. (이메일 기억하기)

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/LoginServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/LoginServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,9 +19,19 @@ public class LoginServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
 
+
     Member m = new Member();
     m.setEmail(request.getParameter("email"));
     m.setPassword(request.getParameter("password"));
+
+    if (request.getParameter("saveEmail") != null) {
+      Cookie cookie = new Cookie("email", m.getEmail());
+      response.addCookie(cookie);
+    } else {
+      Cookie cookie = new Cookie("email", "no");
+      cookie.setMaxAge(0);
+      response.addCookie(cookie);
+    }
 
     Member loginUser = InitServlet.memberDao.findByEmailAndPassword(m);
     if (loginUser != null) {
