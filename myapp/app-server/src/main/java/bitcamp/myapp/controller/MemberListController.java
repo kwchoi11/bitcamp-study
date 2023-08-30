9e7cdd9651216a752d6f7b9fb4a778195877082f
kwchoi11 <kwchoi0216@gmail.com>
2023-08-30 11:55:40 +0900

    2023.08.30 myapp GoF Facade Pattern 적용 (member 적용 완료. board 적용중)

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/MemberListController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/MemberListController.java
@@ -20,9 +20,7 @@ public class MemberListController extends HttpServlet {
 
     MemberDao memberDao = (MemberDao) this.getServletContext().getAttribute("memberDao");
     request.setAttribute("list", memberDao.findAll());
-
-    response.setContentType("text/html;charset=UTF-8");
-    request.getRequestDispatcher("/WEB-INF/jsp/member/list.jsp").include(request, response);
+    request.setAttribute("viewUrl", "/WEB-INF/jsp/member/list.jsp");
   }
 
 }
