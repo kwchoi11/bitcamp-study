953137d047774fb989d89712c906a392632dbebe
kwchoi11 <kwchoi0216@gmail.com>
2023-09-08 20:42:32 +0900

    2023.09.08

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -4,14 +4,17 @@ import bitcamp.myapp.service.MemberService;
 import bitcamp.myapp.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Map;
 
 @Controller
+@RequestMapping("/auth")
 public class AuthController {
 
   {
@@ -21,18 +24,17 @@ public class AuthController {
   @Autowired
   MemberService memberService;
 
-  @RequestMapping("/auth/form")
-  public String form() {
-    return "/WEB-INF/jsp/auth/form.jsp";
+  @GetMapping("form")
+  public void form() {
   }
 
-  @RequestMapping("/auth/login")
+  @PostMapping("login")
   public String login(
           String email,
           String password,
           String saveEmail,
           HttpSession session,
-          Map<String,Object> model,
+          Model model,
           HttpServletResponse response) throws Exception {
 
     if (saveEmail != null) {
@@ -46,7 +48,7 @@ public class AuthController {
 
     Member loginUser = memberService.get(email, password);
     if (loginUser == null) {
-      model.put("refresh", "2;url=form");
+      model.addAttribute("refresh", "2;url=form");
       throw new Exception("회원 정보가 일치하지 않습니다.");
     }
 
@@ -54,7 +56,7 @@ public class AuthController {
     return "redirect:/";
   }
 
-  @RequestMapping("/auth/logout")
+  @GetMapping("logout")
   public String logout(HttpSession session) throws Exception {
     session.invalidate();
     return "redirect:/";
