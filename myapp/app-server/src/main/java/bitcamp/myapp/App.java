977d4566b8eae55b0a7d2b4be2c527e18925e46d
kwchoi11 <kwchoi0216@gmail.com>
2023-09-12 19:47:47 +0900

    2023.09.12 myapp Spring Boot적용 (진행중)

--- myapp/app-server/src/main/java/bitcamp/myapp/App.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/App.java
@@ -4,15 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.util.UrlPathHelper;
 
 @EnableTransactionManagement
 @SpringBootApplication
@@ -22,10 +17,10 @@ public class App implements WebMvcConfigurer {
     SpringApplication.run(App.class, args);
   }
 
-  // @Bean
-  public MultipartResolver multipartResolver() {
-    return new StandardServletMultipartResolver();
-  }
+//  @Bean
+//  public MultipartResolver multipartResolver() {
+//    return new StandardServletMultipartResolver();
+//  }
 
   @Bean
   public ViewResolver viewResolver() {
@@ -36,23 +31,24 @@ public class App implements WebMvcConfigurer {
     return vr;
   }
 
-  @Override
-  public void configurePathMatch(PathMatchConfigurer configurer) {
-    System.out.println("AppConfig.configurePathMatch() 호출됨!");
-    UrlPathHelper pathHelper = new UrlPathHelper();
+//  @Override
+//  public void configurePathMatch(PathMatchConfigurer configurer) {
+//    System.out.println("AppConfig.configurePathMatch() 호출됨!");
+//    UrlPathHelper pathHelper = new UrlPathHelper();
+//
+//    // @MatrixVariable 기능 활성화
+//    pathHelper.setRemoveSemicolonContent(false);
+//
+//    // DispatcherServlet의 MVC Path 관련 설정을 변경한다.
+//    configurer.setUrlPathHelper(pathHelper);
+//  }
 
-    // @MatrixVariable 기능 활성화
-    pathHelper.setRemoveSemicolonContent(false);
+//  @Override
+//  public void addInterceptors(InterceptorRegistry registry) {
+//    System.out.println("AppConfig.addInterceptors() 호출됨!");
+////    registry
+////            .addInterceptor(new MyInterceptor())
+////            .addPathPatterns("/c04_1/**");
+//  }
 
-    // DispatcherServlet의 MVC Path 관련 설정을 변경한다.
-    configurer.setUrlPathHelper(pathHelper);
-  }
-
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    System.out.println("AppConfig.addInterceptors() 호출됨!");
-//    registry
-//            .addInterceptor(new MyInterceptor())
-//            .addPathPatterns("/c04_1/**");
-  }
 }
